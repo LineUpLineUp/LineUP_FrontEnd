348def648f7c02bfa0395dd42dac986ee3274388
migusdn <migusdn@gmail.com>
2023-08-13 22:09:16 +0900

    feat(onboard): add onboarding backgrounds

--- lib/screen/onboard/onboard_page.dart
+++ lib/screen/onboard/onboard_page.dart
@@ -1,28 +1,29 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-abstract class OnboardingPage extends StatelessWidget{
+abstract class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // background(),
-        // blur(),
+        background(),
+        blur(),
         content(context),
       ],
     );
   }
+
   Widget background();
-  Widget blur(){
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.transparent,
-            Colors.black.withOpacity(0.7),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+
+  Widget blur() {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
+        child: Container(
+          color: Color(0x1912171F).withOpacity(0.1),
         ),
       ),
     );
