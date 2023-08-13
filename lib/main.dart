1c5b5dce074de1030f146b1eb5b4b58ad5aeff52
migusdn <migusdn@gmail.com>
2023-08-13 22:07:28 +0900

    feat(main): remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,18 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:line_up_front_end/screen/const/app_theme.dart';
 import 'package:line_up_front_end/screen/home_screen.dart';
 import 'package:line_up_front_end/screen/onboard/onboard_screen.dart';
+import 'package:line_up_front_end/screen/post/posts_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: appTheme,
         home: OnboardingScreen(), // 온보딩 화면을 시작 화면으로 설정
         routes: {
           '/login': (context) => LoginScreen(),
           '/home': (context) => HomeScreen(),
+          '/posts': (context) => PostsScreen(),
         },
     ),
   );
