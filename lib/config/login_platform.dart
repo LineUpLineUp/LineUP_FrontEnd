3d39ac53d39ee4f3338c0651d96ed00d2df735a7
migusdn <migusdn@gmail.com>
2023-08-17 00:35:58 +0900

    feat(login): create Login Platform enum

---
+++ lib/config/login_platform.dart
@@ -0,0 +1,15 @@
+/// @package :
+/// @name : login_platform.dart
+/// @date : 2023/08/17 12:33 AM
+/// @author : hyunwoopark
+/// @version : 1.0.0
+/// @modified :
+///
+import 'package:flutter/material.dart';
+
+enum LoginPlatform {
+  google,
+  apple,
+  kakao,
+  naver,
+}
